dd74e4eff7bebc115a69615a0eab223bbaddf15a
Mac Tester <mactester@Macintosh.local>
2014-09-03 12:27:32 -0400

    Initial Commit

---
+++ LSSimpleKeychainWrapper/LSSimpleKeychainWrapper.h
@@ -0,0 +1,13 @@
+//
+//  LSSimpleKeychainWrapper.h
+//  LSSimpleKeychainWrapper
+//
+//  Created by Mac Tester on 9/3/14.
+//  Copyright (c) 2014 lunaria. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSSimpleKeychainWrapper : NSObject
+
+@end
---
+++ LSSimpleKeychainWrapper/LSSimpleKeychainWrapper.m
@@ -0,0 +1,13 @@
+//
+//  LSSimpleKeychainWrapper.m
+//  LSSimpleKeychainWrapper
+//
+//  Created by Mac Tester on 9/3/14.
+//  Copyright (c) 2014 lunaria. All rights reserved.
+//
+
+#import "LSSimpleKeychainWrapper.h"
+
+@implementation LSSimpleKeychainWrapper
+
+@end
